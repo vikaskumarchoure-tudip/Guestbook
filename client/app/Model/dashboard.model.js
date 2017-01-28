8a0afe38d730e3c9a0aa912c07eae139639dc013
vikaskumarchoure-tudip <vikaskumar.choure@tudip.nl>
2017-01-28 18:36:23 +0530

    Deleted auto generated files
    Deleted all the auto generated files which generate during the compilation.

--- client/app/Model/dashboard.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var DashboardModel = (function () {
-    function DashboardModel() {
-    }
-    return DashboardModel;
-}());
-exports.DashboardModel = DashboardModel;
-//# sourceMappingURL=dashboard.model.js.map
